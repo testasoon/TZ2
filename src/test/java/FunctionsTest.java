eb155f2227e5ba11f799bc16de1bcf674452b784
testasoon <167714439+testasoon@users.noreply.github.com>
2024-05-10 17:35:11 +0500

    Update FunctionsTest.java

--- src/test/java/FunctionsTest.java
+++ src/test/java/FunctionsTest.java
@@ -9,7 +9,7 @@ public class FunctionsTest
         String[] arguments = new String[]{"test_02.txt"};
         
         Functions obj = new Functions(arguments);
-        Functions obj2 = new Functions(arguments2)
+        Functions obj2 = new Functions(arguments2);
         try {
             assertEquals(obj._min(), 1);
             assertEquals(obj._max(), 8494);
@@ -17,7 +17,7 @@ public class FunctionsTest
             assertEquals(obj2._min(), 0);
             assertEquals(obj2._max(), 9);
             assertEquals(obj2._sum(), 26);
-            assertEquals(obj2._mult(), 0)
+            assertEquals(obj2._mult(), 0);
         } catch (Exception e) {
             e.printStackTrace();
         }
